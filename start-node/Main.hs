0e2e9485d95693e446307625ccae0cd53dd90bc4
David Turner <david.turner@elastic.co>
2018-01-21 21:44:46 +0000

    Introduce ElasticsearchNode abstraction to represent a launched ES node

--- start-node/Main.hs
+++ start-node/Main.hs
@@ -2,25 +2,28 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async
 import Control.Concurrent.MVar
+import Control.Concurrent.STM
 import Control.Exception
 import System.Posix.Signals
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
 import Data.Conduit
-import Data.Conduit.Binary hiding (mapM_)
+import Data.Conduit.Binary hiding (mapM_, take)
 import Data.Conduit.Process
 import Data.Maybe
 import Data.Streaming.Process
 import Data.Time
 import System.Directory
 import System.Environment
+import System.Exit
 import System.FilePath
 import System.IO
 import System.Process
@@ -59,6 +62,8 @@ data NodeConfig = NodeConfig
   , ncIsDataNode           :: Bool
   , ncHttpPort             :: Int
   , ncPublishPort          :: Int
+  , ncJavaHome             :: Maybe String
+  , ncUnicastHostPorts     :: [Int]
   }
 
 stdoutPath :: NodeConfig -> FilePath
@@ -73,8 +78,8 @@ nodeWorkingDirectory nc = crWorkingDirectory (ncCurrentRun nc) </> ncName nc
 configDirectory :: NodeConfig -> FilePath
 configDirectory nc = nodeWorkingDirectory nc </> "config"
 
-sourceConfig :: Monad m => [Int] -> NodeConfig -> Producer m B.ByteString
-sourceConfig otherNodePorts nc = mapM_ yieldString
+sourceConfig :: Monad m => NodeConfig -> Producer m B.ByteString
+sourceConfig nc = mapM_ yieldString
   [ "cluster.name: " ++ crName (ncCurrentRun nc)
   , "node.name: " ++ ncName nc
   , "discovery.zen.minimum_master_nodes: 2"
@@ -85,13 +90,36 @@ sourceConfig otherNodePorts nc = mapM_ yieldString
   , "network.host: 127.0.0.1"
   , "http.port: " ++ show (ncHttpPort nc)
   , "transport.tcp.port: " ++ show (ncPublishPort nc)
-  , "discovery.zen.ping.unicast.hosts: " ++ show [ "127.0.0.1:" ++ show p | p <- otherNodePorts ]
+  , "discovery.zen.ping.unicast.hosts: " ++ show [ "127.0.0.1:" ++ show p | p <- ncUnicastHostPorts nc]
   ]
-  where yieldString :: Monad m => String -> Producer m B.ByteString
-        yieldString = yield . T.encodeUtf8 . T.pack . (++ "\n")
 
-makeConfig :: [Int] -> NodeConfig -> IO ()
-makeConfig otherNodePorts nc = do
+yieldString :: Monad m => String -> Producer m B.ByteString
+yieldString = yield . T.encodeUtf8 . T.pack . (++ "\n")
+
+sourceJvmOptions :: Monad m => Producer m B.ByteString
+sourceJvmOptions = mapM_ yieldString
+  [ "-Xms1g"
+  , "-Xmx1g"
+  , "-XX:+UseConcMarkSweepGC"
+  , "-XX:CMSInitiatingOccupancyFraction=75"
+  , "-XX:+UseCMSInitiatingOccupancyOnly"
+  , "-XX:+AlwaysPreTouch"
+  , "-server"
+  , "-Xss1m"
+  , "-Djava.awt.headless=true"
+  , "-Dfile.encoding=UTF-8"
+  , "-Djna.nosys=true"
+  , "-XX:-OmitStackTraceInFastThrow"
+  , "-Dio.netty.noUnsafe=true"
+  , "-Dio.netty.noKeySetOptimization=true"
+  , "-Dio.netty.recycler.maxCapacityPerThread=0"
+  , "-Dlog4j.shutdownHookEnabled=false"
+  , "-Dlog4j2.disable.jmx=true"
+  , "-XX:+HeapDumpOnOutOfMemoryError"
+  ]
+
+makeConfig :: NodeConfig -> IO ()
+makeConfig nc = do
   createDirectoryIfMissing True $ configDirectory nc
 
   runResourceT $ runConduit
@@ -99,11 +127,11 @@ makeConfig otherNodePorts nc = do
     =$= sinkFile   (configDirectory nc </> "log4j2.properties")
 
   runResourceT $ runConduit
-     $  sourceFile "/Users/davidturner/stack-6.1.1/elasticsearch-6.1.1/config/jvm.options"
+     $  sourceJvmOptions
     =$= sinkFile   (configDirectory nc </> "jvm.options")
 
   runResourceT $ runConduit
-     $  sourceConfig otherNodePorts nc
+     $  sourceConfig nc
     =$= sinkFile   (configDirectory nc </> "elasticsearch.yml")
 
 writeToConsole :: MonadIO m => ConduitM B.ByteString B.ByteString m ()
@@ -120,10 +148,74 @@ checkStarted onStarted = awaitForever $ \bs -> do
 
   yield bs
 
+data ElasticsearchNode = ElasticsearchNode
+  { esnHandle    :: StreamingProcessHandle
+  , esnIsStarted :: STM Bool
+  , esnThread    :: Async ExitCode
+  }
+
+runNode :: NodeConfig -> IO ElasticsearchNode
+runNode nodeConfig = do
+  makeConfig nodeConfig
+
+  (  ClosedStream
+   , (sourceStdout, closeStdout)
+   , (sourceStderr, closeStderr)
+   , sph) <- streamingProcess $ 
+        (proc "/Users/davidturner/stack-6.1.1/elasticsearch-6.1.1/bin/elasticsearch" [])
+        { cwd = Just $ crWorkingDirectory $ ncCurrentRun nodeConfig
+        , env = mkEnv $
+            [("ES_PATH_CONF", Just $ configDirectory nodeConfig)
+            ,("JAVA_HOME", ncJavaHome nodeConfig)]
+        }
+
+  saidStartedVar <- newTVarIO False
+
+  nodeThread <- async $
+    withFile (stdoutPath nodeConfig) AppendMode $ \stdoutLog ->
+    withFile (stderrPath nodeConfig) AppendMode $ \stderrLog -> do
+
+    let concurrentConduit = Concurrently . runConduit
+
+    ((), ()) <- runConcurrently ((,)
+        <$> concurrentConduit (sourceStdout =$= writeToConsole =$= checkStarted (atomically $ writeTVar saidStartedVar True)
+                                                               =$= sinkHandle stdoutLog)
+        <*> concurrentConduit (sourceStderr =$= writeToConsole =$= sinkHandle stderrLog))
+      `finally`     (closeStdout >> closeStderr)
+      `onException` terminateProcess (streamingProcessHandleRaw sph)
+
+    waitForStreamingProcess sph
+
+  return $ ElasticsearchNode
+    { esnHandle    = sph
+    , esnIsStarted = readTVar saidStartedVar
+    , esnThread    = nodeThread
+    }
+
+signalNode :: ElasticsearchNode -> Signal -> IO ()
+signalNode ElasticsearchNode{..} signal = withProcessHandle ph $ \case
+  OpenHandle pid -> signalProcess signal pid
+  _ -> return ()
+  where ph = streamingProcessHandleRaw esnHandle
+
+awaitStarted :: ElasticsearchNode -> STM Bool
+awaitStarted ElasticsearchNode{..} = saidStarted `orElse` threadExited
+  where
+  saidStarted = do
+    isStarted <- esnIsStarted
+    if isStarted then return True else retry
+
+  threadExited = waitSTM esnThread >> return False
+
+awaitExit :: ElasticsearchNode -> STM ExitCode
+awaitExit ElasticsearchNode{..} = waitSTM esnThread
+
 main :: IO ()
 main = do
 
   currentRun <- getCurrentRun
+  javaHome <- lookupEnv "JAVA_HOME"
+
   let nodeConfigs
         = [ NodeConfig
             { ncCurrentRun           = currentRun
@@ -132,71 +224,28 @@ main = do
             , ncIsDataNode           = not isMaster
             , ncHttpPort             = 19200 + nodeIndex
             , ncPublishPort          = 19300 + nodeIndex
+            , ncJavaHome             = javaHome
+            , ncUnicastHostPorts     = [ncPublishPort nc | nc <- nodeConfigs, ncIsMasterEligibleNode nc]
             }
           | nodeIndex <- [1..6]
           , let isMaster = nodeIndex <= 3
           ]
 
-  javaHome <- lookupEnv "JAVA_HOME"
+  nodes <- mapM runNode nodeConfigs
+  let faultyNodes = take 2 nodes
 
-  nodeManagers <- forM nodeConfigs $ \nodeConfig -> do
+  startedFlags <- forM nodes $ atomically . awaitStarted
+  unless (and startedFlags) $ error $ "not all nodes started successfully: " ++ show startedFlags
 
-    makeConfig (map ncPublishPort nodeConfigs) nodeConfig
+  threadDelay 10000000
+  forM_ faultyNodes $ \n -> signalNode n sigTSTP
+  threadDelay 100000000
+  forM_ faultyNodes $ \n -> signalNode n sigCONT
+  threadDelay 10000000
+  forM_ faultyNodes $ \n -> do
+    signalNode n sigKILL
+    atomically $ awaitExit n
 
-    async $
-      withFile (stdoutPath nodeConfig) AppendMode $ \stdoutLog ->
-      withFile (stderrPath nodeConfig) AppendMode $ \stderrLog -> do
-
-      saidStartedVar <- newEmptyMVar
-
-      let cp0 = proc "/Users/davidturner/stack-6.1.1/elasticsearch-6.1.1/bin/elasticsearch" []
-          cp = cp0
-            { cwd = Just $ crWorkingDirectory currentRun
-            , env = mkEnv $
-                [("ES_PATH_CONF", Just $ configDirectory nodeConfig)
-                ,("JAVA_HOME", javaHome)]
-            }
-
-          consumerStdout :: Consumer B.ByteString IO ()
-          consumerStdout = writeToConsole =$= checkStarted (putMVar saidStartedVar ())
-                                          =$= sinkHandle stdoutLog
-
-          consumerStderr :: Consumer B.ByteString IO ()
-          consumerStderr = writeToConsole =$= sinkHandle stderrLog
-
-      (  ClosedStream
-       , (sourceStdout, closeStdout)
-       , (sourceStderr, closeStderr)
-       , sph) <- streamingProcess cp
-
-      pid <- withProcessHandle (streamingProcessHandleRaw sph) $ \case
-        OpenHandle pid -> return pid
-        ClosedHandle exitCode -> error $ "Process already exited with code " ++ show exitCode
-
-      let onStartedNode = do
-            readMVar saidStartedVar
-            putStrLn $ "node is started with pid " ++ show pid
-            threadDelay 10000000
-            when (ncIsMasterEligibleNode nodeConfig) $ do
-              putStrLn $ "sending SIGTSTP to pid " ++ show pid
-              signalProcess sigTSTP pid
-            threadDelay 10000000
-            when (ncIsMasterEligibleNode nodeConfig) $ do
-              putStrLn $ "sending SIGCONT to pid " ++ show pid
-              signalProcess sigCONT pid
-            threadDelay 10000000
-            putStrLn $ "sending SIGKILL to pid " ++ show pid
-            signalProcess sigKILL pid
-
-      withAsync onStartedNode $ \_ -> do
-        ((), ()) <-
-          runConcurrently (
-            (,)
-            <$> Concurrently (sourceStdout  $$ consumerStdout)
-            <*> Concurrently (sourceStderr  $$ consumerStderr))
-          `finally` (closeStdout >> closeStderr)
-          `onException` terminateStreamingProcess sph
-        ec <- waitForStreamingProcess sph
-        print $ "Exit code: " ++ show ec
-
-  mapM_ wait nodeManagers
+  forM_ nodes $ \n -> do
+    signalNode n sigTERM
+    atomically $ awaitExit n
